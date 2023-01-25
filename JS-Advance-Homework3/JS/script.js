12fd852325ea8dacc9ac5a37719ec89f5718af90
Ljupchecas <119630492+Ljupchecas@users.noreply.github.com>
2023-01-25 11:21:45 +0100

    Add files via upload

---
+++ JS-Advance-Homework3/JS/script.js
@@ -0,0 +1,19 @@
+let size = document.getElementById('size');
+let color = document.getElementById('color');
+let button = document.getElementById('btn');
+let h1 = document.getElementById('secondH1');
+h1.style.fontSize = '24px';
+
+let changeSize = (h1, textSize) => textSize === '' ? h1.style.fontSize = 24 + 'px' : h1.style.fontSize = textSize + 'px';
+
+let changeColor = (h1, color = 'black') => h1.style.color = color;
+
+button.addEventListener('click', () => {changeSize(h1, size.value),changeColor(h1, color.value) 
+    size.value = '';
+    color.value = '';
+});
+    
+    
+
+
+
